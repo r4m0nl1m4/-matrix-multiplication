4675ee146e0c36ce15ab32baf5f989eed5bea8b6
r4m0nl1m4 <ramon.ccodes@gmail.com>
2020-12-03 15:16:52 -0300

    refactoring

--- matrix-multiplication-parallel.cpp
+++ matrix-multiplication-parallel.cpp
@@ -21,33 +21,31 @@ MPI_Status status;
 
 int main(int argc, char **argv)
 {
-    int N = atoi(argv[1]), numtasks,taskid,numworkers,source,dest,rows,offset,i,j,l,k;
-
+    int N = atoi(argv[1]);
+    int numtasks, taskid;
     double timeStart, timeEnd, executeTime;
-    double a[N][N],b[N][N],c[N][N];
+    int a[N][N],b[N][N],c[N][N];
 
     /* Start parallel computing */
     MPI_Init(&argc, &argv);
 
-    // Getting the Start Time
     timeStart = MPI_Wtime();
 
-    /* Getting the current processes (rank) */
+    /* Gets the current processes (rank) */
     MPI_Comm_rank(MPI_COMM_WORLD, &taskid);
 
-    /* Getting the number of processes (size) */
+    /* Gets the number of processes (size) */
     MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
 
     /* Allocate parallel environment variables */
     bool isMaster;
-
-    numworkers = numtasks-1;
+    int numworkers, source, rows, offset;
 
     isMaster = (taskid == 0);
     if (isMaster) {
         /* Creates Matrix A and Matrix B */
-        double number1 = rand() % 100;
-        double number2 = rand() % 100;
+        int number1 = rand() % 100;
+        int number2 = rand() % 100;
         for (int i=0; i<N; i++ ){
             for (int j=0; j<N; j++ ){
                 a[i][j] = number1;
@@ -55,79 +53,80 @@ int main(int argc, char **argv)
             }
         }
         /* send matrix data to the worker tasks */
+        numworkers = numtasks-1;
         rows = N/numworkers;
         offset = 0;
-        for (dest=1; dest<=numworkers; dest++){
+        /* send matrix data to the worker tasks */
+        for (int dest=1; dest<=numworkers; dest++){
             MPI_Send(&offset, 1, MPI_INT, dest, 1, MPI_COMM_WORLD);
             MPI_Send(&rows, 1, MPI_INT, dest, 1, MPI_COMM_WORLD);
-            MPI_Send(&a[offset][0], rows*N, MPI_DOUBLE,dest,1, MPI_COMM_WORLD);
-            MPI_Send(&b, N*N, MPI_DOUBLE, dest, 1, MPI_COMM_WORLD);
+            MPI_Send(&a[offset][0], rows*N, MPI_INT,dest,1, MPI_COMM_WORLD);
+            MPI_Send(&b, N*N, MPI_INT, dest, 1, MPI_COMM_WORLD);
             offset = offset + rows;
         }
         /* wait for results from all worker tasks */
-        for (i=1; i<=numworkers; i++){
+        for (int i=1; i<=numworkers; i++){
             source = i;
             MPI_Recv(&offset, 1, MPI_INT, source, 2, MPI_COMM_WORLD, &status);
             MPI_Recv(&rows, 1, MPI_INT, source, 2, MPI_COMM_WORLD, &status);
-            MPI_Recv(&c[offset][0], rows*N, MPI_DOUBLE, source, 2, MPI_COMM_WORLD, &status);
+            MPI_Recv(&c[offset][0], rows*N, MPI_INT, source, 2, MPI_COMM_WORLD, &status);
         }
 
-        // Getting the End Time
         timeEnd = MPI_Wtime();
 
-        // Getting the Time interval
         executeTime = timeEnd-timeStart;
         /*
+        // Print results
         cout << endl << " Matrix A: " << endl << endl;
-        for ( i=0; i<N; i++ ){
+        for (int i=0; i<N; i++ ){
           cout << " ";
-          for ( j=0; j<N; j++ ) cout << a[i][j] << " ";
+          for (int j=0; j<N; j++ ) cout << a[i][j] << " ";
           cout << endl;
         }
         cout << endl;
-
         cout << endl << " Matrix B: " << endl << endl;
-        for ( i=0; i<N; i++ ){
+        for (int i=0; i<N; i++ ){
           cout << " ";
-          for ( j=0; j<N; j++ ) cout << b[i][j] << " ";
+          for (int j=0; j<N; j++ ) cout << b[i][j] << " ";
           cout << endl;
         }
         cout << endl;
-
         cout << endl << " Parallel Product Result Matrix: " << endl << endl;
-        for ( i=0; i<N; i++ ){
+        for (int i=0; i<N; i++ ){
           cout << " ";
-          for ( j=0; j<N; j++ ) cout << c[i][j] << " ";
+          for (int j=0; j<N; j++ ) cout << c[i][j] << " ";
           cout << endl;
         }
         cout << endl;
         */
         saveResultReportOnFile("result_report-parallel-runtime.txt", executeTime);
-
-        return 0;
     }
     else { /* Send  local to process 0 */
         source = 0;
         MPI_Recv(&offset, 1, MPI_INT, source, 1, MPI_COMM_WORLD, &status);
         MPI_Recv(&rows, 1, MPI_INT, source, 1, MPI_COMM_WORLD, &status);
-        MPI_Recv(&a, rows*N, MPI_DOUBLE, source, 1, MPI_COMM_WORLD, &status);
-        MPI_Recv(&b, N*N, MPI_DOUBLE, source, 1, MPI_COMM_WORLD, &status);
+        MPI_Recv(&a, rows*N, MPI_INT, source, 1, MPI_COMM_WORLD, &status);
+        MPI_Recv(&b, N*N, MPI_INT, source, 1, MPI_COMM_WORLD, &status);
 
         // Matrix Product
         line vec = getRandVector(N);
+        int l;
         //printVector(vec);
-        for (i=0; i<N; i++ ){
+        for (int i=0; i<N; i++ ){
             l = vec[i]-1;
-            for (j=0; j<rows; j++ ){                
-                for (k=0; k<N; k++ ){
+            for (int j=0; j<rows; j++ ){                
+                for (int k=0; k<N; k++ ){
                     c[l][j] += a[l][k] * b[k][j];
                 }
             }
         }
+
         MPI_Send(&offset, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
         MPI_Send(&rows, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
-        MPI_Send(&c, rows*N, MPI_DOUBLE, 0, 2, MPI_COMM_WORLD);
+        MPI_Send(&c, rows*N, MPI_INT, 0, 2, MPI_COMM_WORLD);
     }
 
     MPI_Finalize();
+
+    return 0;
 }
